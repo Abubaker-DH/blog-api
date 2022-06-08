bb868f93cb20e00223111efd28de2a38bab4c35c
abubaker <abubakerdahab@gmail.com>
2022-06-08 09:07:06 +0200

    add dotenv file, connecte the to mogodb.

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+require("dotenv").config();
 const express = require("express");
 
 const app = express();
 
+const MONGO_URL = process.env.MONGO_URI;
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`server running on port ${PORT} ...`));
+mongoose
+  .connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`server running on port ${PORT} ...`));
+  });
