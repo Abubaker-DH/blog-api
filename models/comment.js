ba59a1e71e714d5cba14ee49b3449c489edf018c
abubaker <abubakerdahab@gmail.com>
2022-06-15 21:49:17 +0200

    add comments route.

--- models/comment.js
+++ models/comment.js
@@ -21,9 +21,8 @@ const commentSchema = new mongoose.Schema(
 
 function validateComment(comment) {
   const schema = Joi.object({
-    text: Joi.string().max(255).required(),
+    text: Joi.string().max(255).min(1).required(),
     articleId: Joi.string().required(),
-    userId: Joi.string().required(),
   });
   return schema.validate(comment);
 }
---
+++ routes/comments.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const { Article } = require("../models/article");
+const { Comment, validateComment } = require("../models/comment");
+const router = express.Router();
+
+// INFO: Get all Comment that belong to article
+router.get("/:articleId", async (req, res) => {
+  const article = await Article.findById({
+    _id: req.params.articleId,
+  });
+
+  if (!article)
+    return res.status(404).send("The Article with given ID was not found.");
+
+  const comments = await Comment.find({
+    articleId: req.params.articleId,
+  }).populate("userId", "-password -email -role");
+
+  res.status(200).json(comments);
+});
+
+// INFO: Create new message Route
+router.post("/", async (req, res) => {
+  const { error } = validateComment(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const article = await Article.findById({
+    _id: req.body.articleId,
+  });
+
+  if (!article)
+    return res.status(404).send("The Aticle with given ID was not found.");
+
+  const newComment = new Comment({
+    articleId: req.body.articleId,
+    text: req.body.text,
+  });
+
+  await newComment.save();
+
+  res.status(201).json(newComment);
+});
+
+// INFO: Delete comment
+router.delete("/:id", async (req, res) => {
+  const comment = await Comment.findById({
+    _id: req.params.id,
+  });
+
+  if (!comment)
+    return res.status(404).send("The Comment with given ID was not found.");
+
+  // NOTE: Only the Admin can delete comment
+
+  await Comment.findByIdAndRemove({
+    _id: req.params.id,
+  });
+
+  res.status(200).json(comment);
+});
+
+module.exports = router;
