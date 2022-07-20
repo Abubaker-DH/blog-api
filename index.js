018e092aa0109eddcd2b5293f9d94846b10cef8e
abubaker <abubakerdahab@gmail.com>
2022-07-20 16:31:28 +0200

    add port 5000

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ require("./startup/production")(app);
 require("./startup/fileUpload")(app);
 require("./startup/routes")(app);
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 const server = app.listen(port, () =>
   winston.info(`Server Listening on port ${port}...`)
 );
