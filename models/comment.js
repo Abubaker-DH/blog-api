4dbbf16e7c9a21dfb3fbb2edd163e2638135cc49
abubaker <abubakerdahab@gmail.com>
2022-07-26 15:55:48 +0200

    add comment model.

---
+++ models/comment.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const commentSchema = new mongoose.Schema(
+  {
+    articleId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Article",
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+function validateComment(comment) {
+  const schema = Joi.object({
+    message: Joi.string().max(255).min(1).required(),
+  });
+  return schema.validate(comment);
+}
+
+module.exports.Comment = mongoose.model("Comment", commentSchema);
+module.exports.validateComment = validateComment;
