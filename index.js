a86c43fb720bab3cb1f8d44736892a31573c6cac
abubaker <abubakerdahab@gmail.com>
2022-07-27 02:31:08 +0200

    add the routes to index file.

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const dotenv = require("dotenv");
 const express = require("express");
 const mongoose = require("mongoose");
+const users = require("../routes/users");
+const categories = require("../routes/categories");
+const articles = require("../routes/articles");
+const comments = require("../routes/comments");
 
 const app = express();
 app.use(express.json());
 dotenv.config();
+
+app.use("/api/v1/articles", articles);
+app.use("/api/v1/categories", categories);
+app.use("/api/v1/comments", comments);
+app.use("/api/v1/users", users);
+app.use(error);
 
 const MONGO_URL = process.env.MONGO_URI;
 const PORT = process.env.PORT || 5000;
