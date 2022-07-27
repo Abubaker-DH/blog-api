ed0bddc9830df8633513674cbdc206c251b7ce80
abubaker <abubakerdahab@gmail.com>
2022-07-27 03:29:29 +0200

    add production packages

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 const dotenv = require("dotenv");
 const express = require("express");
 const mongoose = require("mongoose");
+const Joi = require("joi");
+const cors = require("cors");
+const helmet = require("helmet");
+const compression = require("compression");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
 const users = require("../routes/users");
 const categories = require("../routes/categories");
 const articles = require("../routes/articles");
 const comments = require("../routes/comments");
 
 const app = express();
+Joi.objectId = require("joi-objectid")(Joi);
+
 app.use(express.json());
 dotenv.config();
+
+// INFO: if we behind a proxy
+app.set("trust proxy", 1);
+// INFO: use it to limit number of reqest
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+);
+
+//  INFO: production packages
+app.use(cors());
+app.use(xss());
+app.use(helmet());
+app.use(compression());
 
 // INFO: path for ststic file
 app.use("/images", express.static(path.join(__dirname, "images")));
